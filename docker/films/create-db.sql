8badd505de180b8f7eab259161086f017e87a53b
Craicic <common.hori@protonmail.com>
2023-12-05 13:49:07 +0100

    ignore, add files for project

---
+++ docker/films/create-db.sql
@@ -0,0 +1,5 @@
+CREATE USER db_user WITH PASSWORD 'secret' CREATEDB;
+CREATE DATABASE base_de_donnee_des_films WITH OWNER db_user;
+
+
+
---
+++ docker/voyageurs/create-db.sql
@@ -0,0 +1,2 @@
+CREATE USER db_user WITH PASSWORD 'secret' CREATEDB;
+CREATE DATABASE base_de_donnee_des_voyageurs WITH OWNER db_user;
