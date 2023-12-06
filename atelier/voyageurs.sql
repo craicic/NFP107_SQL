f3ee5fd97f27d3952c205370bad5a4ac0b64e13c
Craicic <common.hori@protonmail.com>
2023-12-06 13:58:32 +0100

    ajout des fichiers exercice pour l'atelier

---
+++ atelier/voyageurs.sql
@@ -0,0 +1,49 @@
+/* Sur notre base des voyageurs en ligne (ou sur la vôtre, après installation d’un SGBD et chargement de nos scripts),
+   vous devez exprimer les requêtes suivantes: */
+
+-- Nom des villes
+
+-- Nom des logements en Bretagne
+
+-- Nom des logements dont la capacité est inférieure à 20
+
+-- Description des activités de plongée
+
+-- Nom des logements avec piscine
+
+-- Nom des voyageurs qui sont allés en Corse
+
+-- Les voyageurs qui sont allés ailleurs qu’en Corse
+
+-- Nom des logements visités par un auvergnat
+
+-- Nom des logements et des voyageurs situés dans la même région
+
+-- Les paires de voyageurs (donner les noms) qui ont séjourné dans le même logement
+
+-- Les voyageurs qui sont allés (au moins) deux fois dans le même logement
+
+-- Les logements qui ont reçu (au moins) deux voyageurs différents
+
+/* Contrainte: n’utilisez pas l’imbrication, pour aucune requête ( et forcez-vous à utiliser la forme déclarative,
+même si vous connaissez d’autres options que nous étudierons dans le prochain chapitre). */
+
+-- Pour les requêtes suivantes, en revanche, vous avez droit à l’imbrication (il serait difficile de faire autrement).
+
+-- Nom des voyageurs qui ne sont pas allés en Corse
+
+-- Noms des voyageurs qui ne vont qu’en Corse s’ils vont quelque part.
+
+-- Nom des logements sans piscine
+
+-- Nom des voyageurs qui ne sont allés nulle part
+
+-- Les logements où personne n’est allé
+
+-- Les voyageurs qui n’ont jamais eu l’occasion de faire de la plongée
+
+-- Les voyageurs et les logements où ils n’ont jamais séjourné
+
+-- Les logements où tout le monde est allé
+
+/* Vous pouvez finalement reprendre quelques-unes de requêtes précédentes et les exprimer avec l’imbrication. */
